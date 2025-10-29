89a1f9180b736677974680f6e9f76341259f4c2b
Dan Garner <dan@xibosignage.com>
2025-10-29 13:53:49 +0000

    Bump client version to 4 R406.0
    Updated ClientVersion, ClientCodeVersion, and assembly version numbers to 4 R406.0 to reflect the new release.

--- Logic/ApplicationSettings.cs
+++ Logic/ApplicationSettings.cs
@@ -52,9 +52,9 @@ namespace XiboClient
         /// </summary>
         private List<string> ExcludedProperties;
 
-        public string ClientVersion { get; } = "4 R405.3";
+        public string ClientVersion { get; } = "4 R406.0";
         public string Version { get; } = "7";
-        public int ClientCodeVersion { get; } = 405;
+        public int ClientCodeVersion { get; } = 406;
 
         private ApplicationSettings()
         {
